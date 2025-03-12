eclipse-rcptt/org.eclipse.rcptt

commit 2d132a197e4f3bbfe5de9ac413803f24e4548e71

Fix overwrite of VM arguments in Q7ExternalLaunchDelegate

launching/org.eclipse.rcptt.launching.ext/src/org/eclipse/rcptt/launching/ext/Q7ExternalLaunchDelegate.java
@@ -525,7 +525,7 @@ public class Q7ExternalLaunchDelegate extends
 		}
 		String override = configuration.getAttribute(
 				IQ7Launch.OVERRIDE_SECURE_STORAGE, (String) null);
-		if (override == null || "true".equals(override)) {
+		if ("true".equals(override)) {
 			// Override existing parameter
 			programArgs.add("-eclipse.keyring");
 			programArgs.add(getConfigDir(configuration).toString()
@@ -552,7 +552,21 @@ public class Q7ExternalLaunchDelegate extends
 			return info.vmArgs;
 		}
 		List<String> args = new ArrayList<String>(Arrays.asList(super.getVMArguments(config)));
+		ITargetPlatformHelper target = (ITargetPlatformHelper) info.target;
 
+		massageVmArguments(config, args, target, launch.getAttribute(IQ7Launch.ATTR_AUT_ID));
+		
+
+		info.vmArgs = args.toArray(new String[args.size()]);
+		Q7ExtLaunchingPlugin.getDefault().info(
+				Q7_LAUNCHING_AUT + config.getName()
+						+ ": AUT JVM arguments is set to : "
+						+ Arrays.toString(info.vmArgs));
+		return info.vmArgs;
+	}
+
+	public static void massageVmArguments(ILaunchConfiguration config, List<String> args, ITargetPlatformHelper target, String autId)
+			throws CoreException {
 		// Filter some PDE parameters
 		Iterables.removeIf(args, new Predicate<String>() {
 			public boolean apply(String input) {
@@ -563,10 +577,9 @@ public class Q7ExternalLaunchDelegate extends
 			}
 		});
 
-		args.add("-Dq7id=" + launch.getAttribute(IQ7Launch.ATTR_AUT_ID));
+		args.add("-Dq7id=" + autId);
 		args.add("-Dq7EclPort=" + AutEventManager.INSTANCE.getPort());
 
-		TargetPlatformHelper target = (TargetPlatformHelper) ((ITargetPlatformHelper) info.target);
 
 		IPluginModelBase hook = target.getWeavingHook();
 		if (hook == null) {
@@ -584,17 +597,9 @@ public class Q7ExternalLaunchDelegate extends
 		args.addAll(vmSecurityArguments(config));
 		
 		args.add("-Declipse.vmargs=" + Joiner.on("\n").join(args) + "\n");
-		
-
-		info.vmArgs = args.toArray(new String[args.size()]);
-		Q7ExtLaunchingPlugin.getDefault().info(
-				Q7_LAUNCHING_AUT + config.getName()
-						+ ": AUT JVM arguments is set to : "
-						+ Arrays.toString(info.vmArgs));
-		return info.vmArgs;
 	}
 	
-	private static List<String> vmSecurityArguments(ILaunchConfiguration configuration) throws CoreException {
+	public static List<String> vmSecurityArguments(ILaunchConfiguration configuration) throws CoreException {
 		// Magic constant from org.eclipse.jdt.internal.launching.environments.ExecutionEnvironmentAnalyzer
 		ArrayList<String> result = new ArrayList<>();
 		Set<String> envs = getMatchingEnvironments(configuration);

rcp/org.eclipse.rcptt.launching.configuration/src/org/eclipse/rcptt/launching/configuration/Q7LaunchConfigurationDelegate.java
@@ -198,19 +198,11 @@ public class Q7LaunchConfigurationDelegate extends
 		LaunchInfoCache.CachedInfo info = LaunchInfoCache.getInfo(config);
 		List<String> args = new ArrayList<String>(Arrays.asList(super
 				.getVMArguments(config)));
+		
+		ITargetPlatformHelper target = (ITargetPlatformHelper) info.target;
+		
+		Q7ExternalLaunchDelegate.massageVmArguments(config, args, target, launch.getAttribute(IQ7Launch.ATTR_AUT_ID));
 
-		args.add("-Dq7id=" + launch.getAttribute(IQ7Launch.ATTR_AUT_ID));
-		args.add("-Dq7EclPort=" + AutEventManager.INSTANCE.getPort());
-
-		IPluginModelBase hook = ((TargetPlatformHelper) info.target)
-				.getWeavingHook();
-		if (hook == null) {
-			throw new CoreException(Q7ExtLaunchingPlugin.status("No "
-					+ AJConstants.HOOK + " plugin"));
-		}
-
-		args.add("-Dosgi.framework.extensions=reference:file:"
-				+ hook.getInstallLocation());
 		info.vmArgs = (String[]) args.toArray(new String[args.size()]);
 		return info.vmArgs;
 	}

commit 8512b44a9f00eb83cf35e7d53f8a02a0d5744af1

Bug 580941: Fix default value handling for OVERRIDE_SECURE_STORAGE
This fixes a regression introduced in
2d132a197e4f3bbfe5de9ac413803f24e4548e71

The default value handling is required, because AUTArgumentsTab
specifically deletes values matching the default.

launching/org.eclipse.rcptt.launching.ext/src/org/eclipse/rcptt/launching/ext/Q7ExternalLaunchDelegate.java
@@ -523,9 +523,8 @@ public class Q7ExternalLaunchDelegate extends
 		} catch (IOException e) {
 			throw new CoreException(Q7ExtLaunchingPlugin.status(e));
 		}
-		String override = configuration.getAttribute(
-				IQ7Launch.OVERRIDE_SECURE_STORAGE, (String) null);
-		if ("true".equals(override)) {
+		if ( configuration.getAttribute(
+				IQ7Launch.OVERRIDE_SECURE_STORAGE, true)) {
 			// Override existing parameter
 			programArgs.add("-eclipse.keyring");
 			programArgs.add(getConfigDir(configuration).toString()

modules/rap/bundles/launching/org.eclipse.rcptt.launching.rap/src/org/eclipse/rcptt/launching/rap/RcpttRapLaunchDelegate.java
@@ -479,9 +479,8 @@ public class RcpttRapLaunchDelegate extends EquinoxLaunchConfiguration {
 		} catch (IOException e) {
 			throw new CoreException(Q7ExtLaunchingPlugin.status(e));
 		}
-		String override = configuration.getAttribute(
-				IQ7Launch.OVERRIDE_SECURE_STORAGE, (String) null);
-		if (override == null || "true".equals(override)) {
+		if (configuration.getAttribute(
+				IQ7Launch.OVERRIDE_SECURE_STORAGE, true)) {
 			// Override existing parameter
 			programArguments.add("-eclipse.keyring");
 			programArguments.add(getConfigDir(configuration).toString()

rcp/org.eclipse.rcptt.launching.configuration/src/org/eclipse/rcptt/launching/configuration/Q7LaunchConfigurationDelegate.java
@@ -247,9 +247,8 @@ public class Q7LaunchConfigurationDelegate extends
 		} catch (IOException e) {
 			throw new CoreException(Q7ExtLaunchingPlugin.status(e));
 		}
-		String override = configuration.getAttribute(
-				IQ7Launch.OVERRIDE_SECURE_STORAGE, (String) null);
-		if ("true".equals(override)) {
+		if (configuration.getAttribute(
+				IQ7Launch.OVERRIDE_SECURE_STORAGE, true)) {
 			// Override existing parameter
 			programArgs.add("-eclipse.keyring");
 			programArgs.add(getConfigDir(configuration).toString()

rcp/org.eclipse.rcptt.launching.ext.ui/src/org/eclipse/rcptt/internal/launching/ext/ui/AUTArgumentsTab.java
@@ -11,6 +11,8 @@
 package org.eclipse.rcptt.internal.launching.ext.ui;
 
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.ILog;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
 import org.eclipse.jdt.debug.ui.launchConfigurations.JavaArgumentsTab;
@@ -33,6 +35,7 @@ import org.eclipse.swt.widgets.Text;
 public class AUTArgumentsTab extends JavaArgumentsTab implements IAUTListener {
 	Text vmArguments;
 	Button secureStorage;
+	private static final ILog LOG = Platform.getLog(AUTArgumentsTab.class);
 
 	@Override
 	protected VMArgumentsBlock createVMArgsBlock() {
@@ -57,10 +60,12 @@ public class AUTArgumentsTab extends JavaArgumentsTab implements IAUTListener {
 			public void initializeFrom(ILaunchConfiguration configuration) {
 				super.initializeFrom(configuration);
 				try {
-					String value = configuration.getAttribute(
-							IQ7Launch.OVERRIDE_SECURE_STORAGE, (String) null);
-					secureStorage.setSelection(value == null);
+					boolean value = configuration.getAttribute(
+							IQ7Launch.OVERRIDE_SECURE_STORAGE, true);
+					secureStorage.setSelection(value);
 				} catch (CoreException e) {
+					LOG.error("FAiled to read " + IQ7Launch.OVERRIDE_SECURE_STORAGE + "attribute"  , e);
+					throw new IllegalArgumentException(e);
 				}
 			}
 
@@ -72,7 +77,7 @@ public class AUTArgumentsTab extends JavaArgumentsTab implements IAUTListener {
 							.removeAttribute(IQ7Launch.OVERRIDE_SECURE_STORAGE);
 				} else {
 					configuration.setAttribute(
-							IQ7Launch.OVERRIDE_SECURE_STORAGE, "false");
+							IQ7Launch.OVERRIDE_SECURE_STORAGE, false);
 				}
 				super.performApply(configuration);
 

runner/org.eclipse.rcptt.runner/src/org/eclipse/rcptt/runner/util/AutThread.java
@@ -192,9 +192,7 @@ public class AutThread extends Thread {
 			config.setAttribute(IQ7Launch.ATTR_HEADLESS_LAUNCH, true);
 			config.setAttribute(DebugPlugin.ATTR_CAPTURE_OUTPUT, true);
 
-			if (!conf.overrideSecurityStorage) {
-				config.setAttribute(IQ7Launch.OVERRIDE_SECURE_STORAGE, "false");
-			}
+			config.setAttribute(IQ7Launch.OVERRIDE_SECURE_STORAGE, !conf.overrideSecurityStorage);
 
 			config.setAttribute(IQ7Launch.ATTR_OUT_FILE, outFilePath);
 			final String vmArgs = Q7LaunchDelegateUtils.getJoinedVMArgs(tpc.getTargetPlatform(),