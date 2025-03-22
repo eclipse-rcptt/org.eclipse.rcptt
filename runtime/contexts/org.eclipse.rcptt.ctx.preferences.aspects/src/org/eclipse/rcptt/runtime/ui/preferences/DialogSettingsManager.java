64c4ca2806534f0058208bdb9009dc00660e54a4
Vasili Gulevich <vasili.gulevich@xored.com>
2025-03-22 15:03:38 +0400

    Bug 549929: Fix dialog settings
    AspectJ support or them was broken in 2020:
    https://github.com/eclipse-platform/eclipse.platform.ui/commit/8266c76c0792ed71ee13bff1514e3c5cab3f4883
    
    Configure
        /settings/org.eclipse.ui.workbench.texteditor/hasShownOverlayPopupBefore=true
    by default

--- runtime/contexts/org.eclipse.rcptt.ctx.preferences.aspects/src/org/eclipse/rcptt/runtime/ui/preferences/DialogSettingsManager.java
+++ runtime/contexts/org.eclipse.rcptt.ctx.preferences.aspects/src/org/eclipse/rcptt/runtime/ui/preferences/DialogSettingsManager.java
@@ -10,18 +10,42 @@
  *******************************************************************************/
 package org.eclipse.rcptt.runtime.ui.preferences;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 import org.eclipse.jface.dialogs.IDialogSettings;
+import org.eclipse.ui.PlatformUI;
 import org.osgi.framework.Bundle;
 
 public class DialogSettingsManager {
 	private static DialogSettingsManager theInstance;
 	private final Map<String, IDialogSettings> settings;
+	private final Function<Bundle, IDialogSettings> GET_DIALOG_SETTINGS;
 
 	private DialogSettingsManager() {
 		settings = new HashMap<String, IDialogSettings>();
+		Function<Bundle, IDialogSettings> temp = null;
+		try {
+			Method getDialogSettingsProvider = PlatformUI.class.getMethod("getDialogSettingsProvider", Bundle.class);
+			Method getDialogSettings = Class.forName("org.eclipse.jface.dialogs.IDialogSettingsProvider").getMethod("getDialogSettings");
+			temp = bundle -> {
+				try {
+					return (IDialogSettings) getDialogSettings.invoke(getDialogSettingsProvider.invoke(null, bundle));
+				} catch (InvocationTargetException | IllegalAccessException e) {
+					throw new IllegalStateException(e);
+				}
+			};
+		} catch (NoSuchMethodException | ClassNotFoundException e) {
+		}
+		if (temp == null) {
+			// Bug 549929: before 2020 no getDialogSettingsProvider() method was available, use AspectJ to intercept AbstractUIPlugin.getDialogSettings()
+			// https://github.com/eclipse-platform/eclipse.platform.ui/commit/8266c76c0792ed71ee13bff1514e3c5cab3f4883
+			temp = bundle -> settings.get(getKey(bundle));
+		}
+		GET_DIALOG_SETTINGS = temp;
 	}
 
 	public static DialogSettingsManager getInstance() {
@@ -30,9 +54,9 @@ public class DialogSettingsManager {
 		}
 		return theInstance;
 	}
-
+	
 	public IDialogSettings getSettings(Bundle bundle) {
-		return settings.get(getKey(bundle));
+		return GET_DIALOG_SETTINGS.apply(bundle);
 	}
 
 	public void addSettings(Bundle bundle, IDialogSettings dialogSettings) {
