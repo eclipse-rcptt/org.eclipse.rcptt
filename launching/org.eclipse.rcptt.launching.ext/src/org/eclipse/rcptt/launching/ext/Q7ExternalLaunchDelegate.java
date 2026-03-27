7e62e639e58a59efae072d4ac7adb1efb490b090
Vasili Gulevich <vasili.gulevich@xored.com>
2026-03-27 21:10:18 +0400

    Fail early if injection fails #258

--- launching/org.eclipse.rcptt.launching.ext/src/org/eclipse/rcptt/launching/ext/Q7ExternalLaunchDelegate.java
+++ launching/org.eclipse.rcptt.launching.ext/src/org/eclipse/rcptt/launching/ext/Q7ExternalLaunchDelegate.java
@@ -520,7 +520,7 @@ public class Q7ExternalLaunchDelegate extends
 		monitor.done();
 	}
 	
-	public static void removeUnresolved(BundlesToLaunch launch) {
+	public static void removeUnresolved(BundlesToLaunch launch) throws CoreException {
 		DependencyResolver resolver = new DependencyResolver(launch.fAllBundles);
 		Collection<IPluginModelBase> toDelete = resolver.checkPlugins(launch.fModels.keySet());
 		toDelete.forEach(plugin->{
@@ -529,7 +529,11 @@ public class Q7ExternalLaunchDelegate extends
 		});
 		String message  = "Following bundles were unresolved:\n" + 
 		toDelete.stream().map(p -> p.getBundleDescription().getName()+"_"+p.getPluginBase().getVersion()).collect(Collectors.joining("\n"));
-		log(Status.info(message));
+		if (launch.fAllBundles.containsKey("org.eclipse.rcptt.core")) {
+			log(Status.info(message));
+		} else {
+			throw new CoreException(Status.error("Injection failed:\n" + message) );
+		}
 	}
 
 	public static class BundlesToLaunchCollector {
